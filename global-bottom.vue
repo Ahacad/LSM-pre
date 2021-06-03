e243786b099059f6c46d66d9e6380e8a54e7b699
ahacad <ahacad@126.com>
2021-06-03 14:38:12 +0800

    feat(footer): add global footer

---
+++ global-bottom.vue
@@ -0,0 +1,9 @@
+<!-- global-bottom.vue -->
+<template>
+  <footer
+    v-if="$slidev.nav.currentLayout !== 'cover'"
+    class="absolute bottom-0 right-0 p-2 text-teal-900"
+  >
+    {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+  </footer>
+</template>
